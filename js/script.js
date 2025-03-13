2730ba7e8db445d82748bf60cfe881c299c5da9e
Dinda Permata Putri <permatad654@gmail.com>
2025-03-13 11:17:24 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 //ini file javascript
+console. log("Javascript is working!");
+
+function validateForm() {
+    // Get value from input
+    const nameInput = document.getElementById('name-input');
+    console.log(nameInput.value);
+
+    if(nameInput.value == '') {
+        alert('Name must be filled out');
+
+    } else {
+        //Write value to the page
+    }
+
+    const birthdateInput = document.getElementById('birthdate-input');
+    console.log(birthdateInput.value);
+
+    if(birthdateInput.value == '') {
+        alert('Date must be choosed');
+
+    } else {
+        //Write value to the page
+    }
+
+}
